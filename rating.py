691d079310c7efe3568a795cbf092a04c9544681
Dima Gerasimov <karlicoss@gmail.com>
2017-05-27 13:59:04 +0100

    prettify rendering code

--- rating.py
+++ rating.py
@@ -4,6 +4,12 @@ import json
 import random
 from typing import Dict, List, Set, Tuple
 
+def invmap(d: Dict) -> Dict:
+    res = {v: k for (k, v) in d.items()}
+    if len(d) != len(d):
+        raise RuntimeError("Dictionary is not bijective!")
+    return res
+
 j = None
 with open('data.json', 'r') as fo:
     j = json.load(fo)
@@ -26,13 +32,11 @@ class Movie:
     def __repr__(self, *args, **kwargs):
         return self.title()
 
-def get_ratings_map(fname: str) -> Dict[str, str]:
-    with open(fname, 'r') as fo:
-        reader = csv.DictReader(fo)
-        res = {}
-        for line in reader:
-            res[line['Title']] = line['You rated'] # TODO ugh, screw float for now...
-        return res
+
+class Movie2:
+    def __init__(self, title: str, rating: str) -> None:
+        self.title = title
+        self.rating = rating
 
 class Ratings:
     def __init__(self, ratings: dict) -> None:
@@ -72,38 +76,114 @@ sym2edge = {
     'i': Edge.IGNORE,
 }
 
-edge2sym = {
-    v: k for k, v in sym2edge.items()
-}
+edge2sym = invmap(sym2edge)
+
+class RatingGraph:
+
+    def __init__(self, id2movie: Dict[str, Movie2], graph: Dict[str, List[str]]) -> None:
+        self.id2movie = id2movie
+        self.graph = graph
+
+    @classmethod
+    def load(cls, imdb_fname: str, ratings_fname: str):
+        id2movie = {}
+        with open(imdb_fname, 'r') as fo:
+            reader = csv.DictReader(fo)
+            for i, line in enumerate(reader):
+                title = line['Title']
+                rating = line['You rated']
+                id_ = "n" + str(i)
+                id2movie[id_] = Movie2(title, rating)
+
+        title2id = invmap({k: v.title for k, v in id2movie.items()})
+        graph = {id_: [] for id_ in id2movie}
+        with open(ratings_fname, 'r') as fo:
+            for line in fo:
+                [st, a, b] = line.split(';')
+                st = st.strip()
+                a = a.strip()
+                b = b.strip()
+                ida = title2id[a]
+                idb = title2id[b]
+                edge = None
+                if st not in sym2edge:
+                    print("State for {}; {} is invalid!".format(a, b))
+                else:
+                    edge = sym2edge[st]
+                if edge == Edge.WORSE:
+                    graph[ida].append(idb)
+                elif edge == Edge.BETTER:
+                    graph[idb].append(ida)
+                else:
+                    pass # can't do mych at this point?
+        return cls(id2movie, graph)
 
 
+    # TODO tred: transitive reduction
+    # dot -Tpng graph.dot  -o graph.png 
+    # TODO update dynamically, you can see the nodes moving and laying out.
+    def plot(self, dot_fname: str) -> None:
+        for id_ in sorted(self.id2movie.keys(), key=lambda s: int(s[1:])):
+            print(id_, self.id2movie[id_].title)
 
-# movies = [Movie(v) for k, v in sorted(titles.items())]
+        dot_contents = ""
+        def app(s):
+            nonlocal dot_contents
+            dot_contents += s + "\n"
+        app('digraph test {')
+        app('rankdir=BT;')
+        app('size="20, 5";')
+        app('dpi="500";')
+        app('ratio="fill";')
 
-# gen.shuffle(movies)
+        edges = []
+        for from_, tos in sorted(self.graph.items()):
+            for to in tos:
+                edges.append('  {} -> {};'.format(from_, to))
+        for e in edges:
+            app(e)
+        # dsu = Dsu(mmap.values())
+            # elif st == Edge.SAME:
+            #     ma = mmap[a]
+            #     mb = mmap[b]
+            #     # dsu.merge(ma, mb)
 
-# sample = movies[:5]
+        vcolor = {}
+        # groups = dsu.get_groups()
+        # groups = {k: v for (k, v) in groups.items() if len(v) > 1}
+        # colors = ['yellow', 'red', 'green', 'blue', 'magenta']
+        # for col, g in zip(colors, groups.values()):
+        #     for v in g:
+        #         vcolor[v] = col
 
-# def compare_prompt():
-#     pass
+        rat2color = {
+            "10": "red",
+            "9": "green",
+            "8": "blue",
+            "7": "yellow",
+        }
+        for id_, movie in self.id2movie.items():
+            vcolor[id_] = rat2color.get(movie.rating, 'white')
 
-def load_state(fname: str) -> Dict[Tuple[str, str], Edge]:
-    with open(fname, 'r') as fo:
-        res = {} # type: Dict[Tuple[str, str], Edge]
-        for line in fo:
-            [st, a, b] = line.split(';')
-            st = st.strip()
-            a = a.strip()
-            b = b.strip()
-            if st not in ['>', '<', '?', '=', 'i']:
-                print("State for {}; {} is invalid!".format(a, b))
-                res[(a, b)] = None
-            else:
-                res[(a, b)] = sym2edge[st]
-        return res
+        for id_ in self.graph:
+            # label = name
+            # rating = ratings[id2name[id_]]
+            label = id_
+
+            fillc = vcolor.get(id_, 'white')
+
+            app('  {} [shape=circle, fillcolor={} style=filled label = "{}"];'.format(id_, fillc, label))
+
+        app('}')
+        with open(dot_fname, 'w') as fo:
+            fo.write(dot_contents)
+
+state_fname = 'state.txt'
+ratings_fname = 'ratings.csv'
+graph = RatingGraph.load(ratings_fname, state_fname)
 
 class Dsu:
-    def __init__(self, items: List):
+    def __init__(self, items: List) -> None:
         self.parents = {i: i for i in items}
 
     def merge(self, i, j):
@@ -130,87 +210,6 @@ class Dsu:
             groups[rp] = l
         return groups
 
-def plot():
-    state_fname = 'state.txt'
-    ratings_fname = 'ratings.csv'
-    old_state = load_state(state_fname)
-    ratings = get_ratings_map(ratings_fname)
-    all_movies = list(sorted(ratings.keys())) # TODO use movie ids instead?..
-    stats = {m: 0 for m in all_movies}
-    for (a, b), s in old_state.items():
-        # TODO only count if not ignored?
-        if s in [Edge.BETTER, Edge.WORSE, Edge.SAME]:
-            stats[a] += 1
-            stats[b] += 1
-    marked = list(sorted(m for (m, c) in stats.items() if c > 0))
-    mmap = {}
-    for i, m in enumerate(marked):
-        mmap[m] = "n" + str(i)
-        print(mmap[m], m)
-
-# TODO tred: transitive reduction
-# dot -Tpng graph.dot  -o graph.png 
-
-# TODO color based on rating?
-# TODO update dynamically, you can see the nodes moving and laying out.
-
-    with open('graph.dot', 'w') as fo:
-        fo.write('digraph test {')
-
-        fo.write('rankdir=BT;\n')
-        fo.write('size="20, 5";\n')
-        fo.write('dpi="500";\n')
-        fo.write('ratio="fill";\n')
-        edges = []
-        dsu = Dsu(mmap.values())
-        for (a, b), st in old_state.items(): # TODO sorted order..
-            if st == Edge.WORSE:
-                ma = mmap[a]
-                mb = mmap[b]
-                edges.append('  {} -> {};'.format(ma, mb))
-            elif st == Edge.BETTER:
-                ma = mmap[a]
-                mb = mmap[b]
-                edges.append('  {} -> {};\n'.format(mb, ma))
-            elif st == Edge.SAME:
-                ma = mmap[a]
-                mb = mmap[b]
-                dsu.merge(ma, mb)
-
-        for e in edges:
-            fo.write(e + "\n")
-
-        id2name = {v:k for k, v in mmap.items()}
-
-        vcolor = {}
-        # groups = dsu.get_groups()
-        # groups = {k: v for (k, v) in groups.items() if len(v) > 1}
-        # colors = ['yellow', 'red', 'green', 'blue', 'magenta']
-        # for col, g in zip(colors, groups.values()):
-        #     for v in g:
-        #         vcolor[v] = col
-
-        rat2color = {
-            "10": "red",
-            "9": "green",
-            "8": "blue",
-            "7": "yellow",
-        }
-        for name, id_ in mmap.items():
-            rating = ratings[id2name[id_]]
-            vcolor[id_] = rat2color.get(rating, 'white')
-
-        for name, id_ in mmap.items():
-            # label = name
-            rating = ratings[id2name[id_]]
-            label = id_ + " " + rating
-
-            fillc = vcolor.get(id_, 'white')
-
-            fo.write('  {} [shape=circle, fillcolor={} style=filled label = "{}"];\n'.format(id_, fillc, label))
-
-        # TODO rest of edges?
-        fo.write('}')
 
 def add_more(seed: int):
     state_fname = 'state.txt'
@@ -251,7 +250,7 @@ def add_more(seed: int):
         for a, b in tsample:
             fo.write(" ;{};{}\n".format(a, b)) # TODO separator?
 
-plot()
+graph.plot('graph.dot')
 # add_more(43676737)
 
 # TODO Fake edges to enforce lower bound on marks
